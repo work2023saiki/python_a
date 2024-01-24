9e37b0ce364d387ce083dfb135821c3a662b776d
03yayama25 <03yayama25@gmail.com>
2024-01-24 15:29:37 +0900

    app.py add

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/') 
+def index():
+    return render_template('top.html')
+
+@app.route('/puzzle') 
+def puzzle():
+    return render_template('15puzzle.html')
+
+@app.route('/Shooting') 
+def Shooting():
+    return render_template('Shooting.html')
+
+@app.route('/Cards') 
+def Cards():
+    return render_template('FlipCards.html')
+
+@app.route('/reversi') 
+def Reversi():
+    return render_template('ReversiblePiece.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
