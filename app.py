1b6c04e344f4520aa64f02f08755a3fcdddba762
atknjm <at.knjm@gmail.com>
2024-01-26 14:53:31 +0900

    4th commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
- yayama
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -28,26 +27,9 @@ def Reversi():
 def Dungeon():
     return render_template('Dungeon.html')
 
-
-
-import os
-from flask import Flask, render_template
-
-
-app = Flask(__name__)
-
-@app.route('/') 
-def index():
-    return render_template('ボス猫.html')
-
-@app.route('/puzzle') 
-def puzzle15():
-    return render_template('15puzzle.html')
-
-@app.route('/saturnvoyager') 
-def saturnvoyager():
-    return render_template('saturnvoyager.html')
- main
+@app.route('/flipcards') 
+def flipcards():
+    return render_template('FlipCards.html')
 
 if __name__ == '__main__':
     app.run()
