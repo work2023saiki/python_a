fa4d3117168e4d95bc75c5d04114a86b854f46db
03yayama25 <03yayama25@gmail.com>
2024-02-06 15:35:17 +0900

    login add

--- app.py
+++ app.py
@@ -50,61 +50,61 @@ def top():
 
 @app.route('/puzzle') 
 def puzzle():
-    return render_template('game/15puzzle.html')
+    return render_template('login/templates/game/15puzzle.html')
 
 @app.route('/Shooting') 
 def Shooting():
-    return render_template('game/Shooting.html')
+    return render_template('login/templates/game/Shooting.html')
 
 @app.route('/Cards') 
 def Cards():
-    return render_template('game/FlipCards.html')
+    return render_template('login/templates/game/FlipCards.html')
 
 @app.route('/reversi') 
 def Reversi():
-    return render_template('game/ReversiblePiece.html')
+    return render_template('login/templates/game/ReversiblePiece.html')
 
 @app.route('/Dungeon') 
 def Dungeon():
-    return render_template('game/Dungeon.html')
+    return render_template('login/templates/game/Dungeon.html')
 
 
 @app.route('/FunkyBlocks') 
 def FunkyBlocks():
-    return render_template('game/FunkyBlocks.html')
+    return render_template('login/templates/game/FunkyBlocks.html')
 
 
   
 @app.route('/Jumper') 
 def Jumper():
-    return render_template('game/Jumper.html')
+    return render_template('login/templates/game/Jumper.html')
 
 @app.route('/CarryIt') 
 def CarryIt():
-    return render_template('game/CarryIt.html')
+    return render_template('login/templates/game/CarryIt.html')
 
   
 
 @app.route('/saturnvoyager') 
 def saturnvoyager():
-    return render_template('game/saturnvoyager.html')
+    return render_template('login/templates/game/saturnvoyager.html')
 
 @app.route('/EggCatch') 
 def EggCatch():
-    return render_template('game/EggCatch.html')
+    return render_template('login/templates/game/EggCatch.html')
 
 
 @app.route('/chase') 
 def chase():
-    return render_template('game/Chase.html')
+    return render_template('login/templates/game/Chase.html')
 
 @app.route('/Billiard') 
 def Billiard():
-    return render_template('game/Billiard.html')
+    return render_template('login/templates/game/Billiard.html')
 
 @app.route('/yasai') 
 def yasai():
-    return render_template('game/yasai.html')
+    return render_template('login/templates/game/yasai.html')
 
 if __name__ == '__main__':
     app.run()
