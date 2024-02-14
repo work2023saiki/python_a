981ffd8663720178e3a518d231a16acbcdffb2c9
kuro8maru9 <saiki>
2024-02-14 15:41:28 +0900

    add MySQL ninsyo

--- app.py
+++ app.py
@@ -1,115 +1,119 @@
-import os    # 乱数を設定するためインポート
-from flask import Flask, render_template, session, redirect, url_for
-from forms import LoginForm, RegisterForm
+from flask import Flask, render_template, url_for
+from flask_login import login_required, LoginManager
+from models import db, User
+from flask_migrate import Migrate
+from auth.views import auth_bp       # authは「authentication」の略。日本語に訳すと「認証」。
+import MySQLdb
 
 app = Flask(__name__)
 
-# 乱数を設定
-app.config['SECRET_KEY'] = os.urandom(24)
+# config.py読み込み
+app.config.from_object("config.Config")
+# データベースとFlaskとの紐づけ
+db.init_app(app)
+# マイグレーションとの紐づけ（Flaskとdb）
+migrate = Migrate(app, db)
+# LoginManagerインスタンス
+login_manager = LoginManager()
+# LoginManagerとの紐づけ
+login_manager.init_app(app)
+# ログインが必要なページにアクセスしようとしたときに表示されるメッセージ
+login_manager.login_message = "認証していません：ログインしてください"
+# 未認証のユーザーがアクセスしようとした際に
+# リダイレクトされる関数名を設定する
+login_manager.login_view = "auth.login"
 
-# [index] index.htmlへのリンク
+# Blueprintをアプリケーションに登録
+app.register_blueprint(auth_bp)
+
+@login_manager.user_loader   #ユーザー情報を読み込む関数として「Flask-Login」に登録
+def load_user(user_id):
+    return User.query.get(int(user_id))    #user_idに対応するユーザー情報を取得
+
+
+
+
 @app.route("/")
-def index():
+def hello():
     return render_template("index.html")
 
-    """
-    ！！！以下ログイン関連ルーティング記述！！！
-    ！！！フォルダ場所指定後、パス変更必須！！！
-    ！！！おわったらけしてね！！！
-    """
-#ログイン
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form2 = LoginForm()      #ファイルforms.pyのLoginFormクラスからオブジェクトを作る
-    # POST
-    if form2.validate_on_submit():    #validatorsの内容が表示されないなら、  
-        return redirect(url_for('top'))     #redirectとすることで、ユーザー名、パスワードの二重送信を防ぐ。
-    # GETリクエストで、ifが通らなかった場合。form2オブジェクトを引数としてformにわたす。
-    return render_template('login.html', form=form2)    #ログイン画面へ
 
-# 登録
-@app.route('/touroku', methods=['GET', 'POST'])
-def touroku():
-    form = RegisterForm()     #ファイルforms.pyのRegisterFormクラスからオブジェクトを作る
-        #ファイルforms.pyのRegisterFormクラスからオブジェクトを作る
-    # POST
-    if form.validate_on_submit():    #validatorsの内容が表示されないなら、
-        session['name'] = form.name.data           #セッションとして保存。登録完了ページで使う。
-        session['password'] = form.password.data     
-        
-        return redirect(url_for('tourokuOK'))   #redirectとすることで、ユーザー名、パスワードの二重送信を防ぐ。PRGパターンのR。
-    
-    # GETリクエストで、ifが通らなかった場合。formオブジェクトを引数としてformにわたす。
-    return render_template('touroku.html', form=form)
-
-# 登録完了画面
-@app.route('/tourokuOK')
-def tourokuOK():
-    return render_template('tourokuOK.html')
-
-    """
-    ！！！ログイン関連ここまで！！！
-    ！！！おわったらけしてね！！！
-    """
- 
-# top.htmlへのリンク
 @app.route('/top')
+@login_required
 def top():
     return render_template('top.html')    
 
-# 以下game
-@app.route('/puzzle') 
+
+@app.route('/puzzle')
+@login_required 
 def puzzle():
     return render_template('game/15puzzle.html')
 
-@app.route('/Shooting') 
+@app.route('/Shooting')
+@login_required 
 def Shooting():
     return render_template('game/Shooting.html')
 
-@app.route('/Cards') 
+@app.route('/Cards')
+@login_required 
 def Cards():
     return render_template('game/FlipCards.html')
 
-@app.route('/reversi') 
+@app.route('/reversi')
+@login_required 
 def Reversi():
     return render_template('game/ReversiblePiece.html')
 
-@app.route('/Dungeon') 
+@app.route('/Dungeon')
+@login_required 
 def Dungeon():
     return render_template('game/Dungeon.html')
 
-@app.route('/FunkyBlocks') 
+
+@app.route('/FunkyBlocks')
+@login_required 
 def FunkyBlocks():
     return render_template('game/FunkyBlocks.html')
 
-@app.route('/Jumper') 
+
+  
+@app.route('/Jumper')
+@login_required 
 def Jumper():
     return render_template('game/Jumper.html')
 
-@app.route('/CarryIt') 
+@app.route('/CarryIt')
+@login_required 
 def CarryIt():
     return render_template('game/CarryIt.html')
 
-@app.route('/saturnvoyager') 
+  
+
+@app.route('/saturnvoyager')
+@login_required 
 def saturnvoyager():
     return render_template('game/saturnvoyager.html')
 
-@app.route('/EggCatch') 
+@app.route('/EggCatch')
+@login_required 
 def EggCatch():
     return render_template('game/EggCatch.html')
 
-@app.route('/chase') 
+
+@app.route('/chase')
+@login_required 
 def chase():
     return render_template('game/Chase.html')
 
-@app.route('/Billiard') 
+@app.route('/Billiard')
+@login_required 
 def Billiard():
     return render_template('game/Billiard.html')
 
-@app.route('/yasai') 
+@app.route('/yasai')
+@login_required 
 def yasai():
     return render_template('game/yasai.html')
 
-# 直接実行された場合に、Flaskアプリケーションを実行し、サーバーを起動する記述
 if __name__ == '__main__':
     app.run()
--- forms.py
+++ forms.py
@@ -1,22 +1,32 @@
 from flask_wtf import FlaskForm                   
 from wtforms import StringField, EmailField, SubmitField, PasswordField     
-from wtforms.validators import DataRequired, Email, Length, EqualTo    #バリデーターを使うためインポート
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError   
+from models import User
 
-#pip install flask-wtf==1.1.1 
-
-# ==================================================
-# Formクラス
-# ==================================================
 # ログイン画面    
 class LoginForm(FlaskForm):
     name = StringField('名前：', validators=[DataRequired('必須入力です')])
     password = PasswordField('パスワード：', validators=[Length(3, 10, 'パスワードの長さは3文字以上10文字以内です')])
     
     submit = SubmitField('ログイン')
+    
+    # バリデータ作成
+    # 英数字が含まれているかチェックする
+    def validate_password(self, password):
+        if not (any(c.isalpha() for c in password.data) and \
+            any(c.isdigit() for c in password.data) ):     #c.isalphaはアルファベット、c.isdigitは数字
+            raise ValidationError('パスワードには英数字を含める必要があります')
+
+
 
 # 新規登録画面
-class RegisterForm(LoginForm):   #ログイン画面のLoginFormクラスを継承する
+class RegisterForm(LoginForm):   #継承
     confirm_password = PasswordField('パスワード確認：', validators=[EqualTo('password', "パスワードが一致しません")])
     
-    submit = SubmitField('送信')   #送信ボタン  オーバーライドしてる
+    submit = SubmitField('送信')   #オーバーライド
 
+    # バリデータ作成
+    def validate_name(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('そのユーザー名は既に使用されています')
