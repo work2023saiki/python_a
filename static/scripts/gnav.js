e8d913c3e809e0204e18fcc2b7a4dc9272bc684d
M.Miyahara <minami.miyahara@gmail.com>
2024-02-26 15:25:05 +0900

    add css and responsive

---
+++ static/scripts/gnav.js
@@ -0,0 +1,26 @@
+//ドロップダウンの設定を関数でまとめる
+function mediaQueriesWin(){
+	var width = $(window).width();
+	if(width <= 768) {//横幅が768px以下の場合 $(".gnav>a").off('click');	//gnavクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
+		$(".gnav>a").on('click', function() {//gnavクラスがついたaタグをクリックしたら
+			var parentElem =  $(this).parent();// aタグから見た親要素のliを取得し
+			$(parentElem).toggleClass('active');//矢印方向を変えるためのクラス名を付与して
+			$(parentElem).children('ul').stop().slideToggle(500);//liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
+			return false;//リンクの無効化
+		});
+	}else{//横幅が768px以上の場合
+		$(".gnav>a").off('click');//gnavクラスがついたaタグのonイベントをoff(無効)にし
+		$(".gnav").removeClass('active');//activeクラスを削除
+		$('.gnav').children('ul').css("display","");//スライドトグルで動作したdisplayも無効化にする
+	}
+}
+
+// ページがリサイズされたら動かしたい場合の記述
+$(window).resize(function() {
+	mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load',function(){
+	mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
+});
